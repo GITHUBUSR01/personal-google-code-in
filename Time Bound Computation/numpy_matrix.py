0a3a727d1e9c4fb75834a0337b3f19b3a24818d8
gauthamk11 <42304949+GITHUBUSR01@users.noreply.github.com>
2019-12-26 11:06:11 -0800

    Multiplying Martices via Numpy

--- Time Bound Computation/numpy_matrix.py
+++ Time Bound Computation/numpy_matrix.py
@@ -6,8 +6,8 @@ A = np.random.randint(10, size=(1000, 10-0))
 B = np.random.randint(10, size=(1000, 100-))
 
 
-Z = np.dot(A, B)
+C = np.dot(A, B)
 print("Multiplying lists with numpy: ")
-print(Z)
+print(C)
 
 print(f"This program takes {time.time() - start_time} seconds to run.")
