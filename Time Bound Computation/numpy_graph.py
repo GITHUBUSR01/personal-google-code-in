6712ee1eec635fc4801d542093238d63c56054db
gauthamk11 <42304949+GITHUBUSR01@users.noreply.github.com>
2019-12-27 10:29:44 -0800

    NumPy Graph

--- Time Bound Computation/numpy_graph.py
+++ Time Bound Computation/numpy_graph.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 
 
 #x and y for NumPy python
-a = [1,5,10,50, 100, 500, 1000]
-b = [0.000768899917602539, 0.00258779525756835, 0.00220608711242675, 0.00296616554260253, 0.005051851272583, 0.784027099609375, 8.45831394195556]
+x = [1,5,10,50, 100, 500, 1000]
+y = [0.000768899917602539, 0.00258779525756835, 0.00220608711242675, 0.00296616554260253, 0.005051851272583, 0.784027099609375, 8.45831394195556]
 
 #plotting the graph
-plt.plot(a, b, 'b-',label='NumPy Python', marker='o', color='b')
+plt.plot(x, y, 'x-',label='NumPy Python', marker='o', color='b')
 
 # labeling the x axis
-plt.xlabel('Dimension of Matrix (n*n)')
+plt.xlabel('Dimension of Matrix (n x n)')
 # labeling the y axis
 plt.ylabel('Time (in seconds)')
 
